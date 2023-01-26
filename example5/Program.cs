e41410051b4fec4a896fdd6ef66c8e72f488b1ed
vlad <norg.vl@mail.ru>
2023-01-26 13:43:20 +0300

    add if

---
+++ example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Type user name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura! It is Masha!");
+
+}
+else
+{
+    Console.WriteLine("Hello, " + username);
+}
