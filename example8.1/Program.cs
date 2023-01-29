9dd1c1d4fa98f49dea9088c26ab1c4d1e51a1877
vlad <norg.vl@mail.ru>
2023-01-29 19:34:55 +0300

    example8.1

---
+++ example8.1/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 4;
+int b = 5;
+int c = 12;
+int d = 76;
+int f = 98;
+int g = 9;
+int h = 0;
+int i = 13;
+int j = -10;
+
+int maxNumber(int a, int b, int c)
+{
+    int max = a;
+    if (b>max) max = b;
+    if (c>max) max = c;
+    return max;
+}
+
+int finalMax = maxNumber(maxNumber(a,b,c), maxNumber(d,f,g), maxNumber(h, i, j));
+
+Console.WriteLine(finalMax);
