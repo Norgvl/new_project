b9aa08b8e0b53682398972cee2ddf41f144222d6
vlad <norg.vl@mail.ru>
2023-01-29 19:16:49 +0300

    example8

---
+++ example8/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] arr = new int[9];
+for (int a = 0; a < 9; a++)
+{
+    arr[a] = new Random().Next(0, 100);
+    Console.Write(arr[a] + " ");
+}
+
+int maxNumber(int[] arr)
+{
+    int max = arr[0];
+    int i = 0;
+    while (i < 9)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        i++;
+
+    }
+    return max;
+}
+
+int max = maxNumber(arr);
+Console.WriteLine(" ");
+Console.WriteLine(max);
